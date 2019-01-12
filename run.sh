2abeb55af8be61432917bea60dcefe5ba857fb62
ResidentMario <aleksey.bilogur@gmail.com>
2019-01-11 16:01:59 -0800

    Restore to normal.

--- run.sh
+++ run.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-if [ ! "$AWS_ACCESS_KEY_ID" = "" ]; then aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID; fi
-if [ ! "$AWS_SECRET_ACCESS_KEY" = "" ]; then aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY; fi
+# TODO: temorarily removed here, is this relevant though?
+# if [ ! "$AWS_ACCESS_KEY_ID" = "" ]; then aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID; fi
+# if [ ! "$AWS_SECRET_ACCESS_KEY" = "" ]; then aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY; fi
 
 if [[ "$1" = train ]]
 then
-    python -c "from sagemaker import get_execution_role; get_execution_role()"
     jupyter nbconvert --execute --ExecutePreprocessor.timeout=-1 --to notebook build.ipynb
 else
     python -c "import t4; t4.Package.install('aleksey/fashion-mnist-clf', registry='s3://alpha-quilt-storage', dest='.')"
