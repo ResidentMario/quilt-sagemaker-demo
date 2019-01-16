c906972d8d31b866f91155cfe6987f961ccf9ea9
ResidentMario <aleksey.bilogur@gmail.com>
2019-01-16 14:47:15 -0800

    Listen on 8080.

--- run.sh
+++ run.sh
@@ -6,5 +6,5 @@ else
     python -c "import t4; t4.Package.install('aleksey/fashion-mnist-clf', registry='s3://alpha-quilt-storage', dest='.')"
     cp aleksey/fashion-mnist-clf/clf.h5 clf.h5
     rm -rf aleksey/
-    python -m flask run --host=0.0.0.0
+    python -m flask run --host=0.0.0.0 --port=8080
 fi
