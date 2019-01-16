d26e78d6205c93f8f982a00e550c3b4a3358f4d9
ResidentMario <aleksey.bilogur@gmail.com>
2019-01-15 22:29:44 -0800

    Fixes.

--- Dockerfile
+++ Dockerfile
@@ -15,12 +15,6 @@ COPY "build.ipynb" .
 COPY "catalog-screencap.png" .
 COPY "health-check-data.csv" .
 
-# install model dependencies
-# following Docker data model and AWS security best practices means pulling the necessary data at runtime
-ENV AWS_ACCESS_KEY_ID=""
-ENV AWS_SECRET_ACCESS_KEY=""
-ENV AWS_REGION=us-east-1
-
 # provision environment
 ENV FLASK_APP app.py
 RUN ["chmod", "+x", "./run.sh"]
--- run.sh
+++ run.sh
@@ -1,8 +1,4 @@
 #!/bin/bash
-# TODO: temorarily removed here, is this relevant though?
-# if [ ! "$AWS_ACCESS_KEY_ID" = "" ]; then aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID; fi
-# if [ ! "$AWS_SECRET_ACCESS_KEY" = "" ]; then aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY; fi
-
 if [[ "$1" = train ]]
 then
     jupyter nbconvert --execute --ExecutePreprocessor.timeout=-1 --to notebook build.ipynb
